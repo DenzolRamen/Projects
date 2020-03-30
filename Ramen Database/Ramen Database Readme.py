9107d5b7cc981c75e15790c46037c92773edaae7
DenzolRamen <62255045+DenzolRamen@users.noreply.github.com>
2020-03-30 13:58:40 +0800

    Update Ramen Database Readme.py

--- Ramen Database/Ramen Database Readme.py
+++ Ramen Database/Ramen Database Readme.py
@@ -1,8 +1,15 @@
-'''
+''' INTRODUCTION
 Hello, this is a project from Denzol!
 I will create a Ramen Database by using Python, tkinter (gui), and sqlite3
 I will use this app to store data that I collected when I will check out the Ramen Shops here in BGC, Taguig,
 after the implementation of the lockdown here in the Philippines.
 '''
+
+# Build Phase Status
+# ============================== #
+# Build Phase: Phase 1           #
+# Job: Creating the Layout       #
+# Date: 3/30/2020 | 1:56 PM PST  #
+# ============================== #
 
 print('Ramen is love')
