47f2d70ff5d05180e4a4607f100c70c7c2db0db6
DenzolRamen <62255045+DenzolRamen@users.noreply.github.com>
2020-03-30 10:40:42 +0800

    Create Ramen Database Readme.py

---
+++ Ramen Database/Ramen Database Readme.py
@@ -0,0 +1,8 @@
+'''
+Hello, this is a project from Denzeol!
+I will create a Ramen Database by using Python, tkinter (gui), and sqlite3
+I will use this app to store data that I collected when I will check out the Ramen Shops here in BGC, Taguig,
+after the supposed lockdown
+'''
+
+print('Ramen is love')
